b4a11d436804f1d1ae2fbecf64f41425c21a71bb
Kyle <kylebascomb@gmail.com>
2022-10-04 21:09:21 -0700

    blackened

--- genecomb/docs/source/conf.py
+++ genecomb/docs/source/conf.py
@@ -6,27 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'genecomb'
-copyright = '2022, Kyle Bascomb'
-author = 'Kyle Bascomb'
-release = '1.0'
+project = "genecomb"
+copyright = "2022, Kyle Bascomb"
+author = "Kyle Bascomb"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser',
-        'sphinx.ext.duration',
-        'sphinx.ext.doctest',
-        'sphinx.ext.autodoc',
-        'sphinx.ext.autosummary']
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
